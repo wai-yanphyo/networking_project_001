4aeb30d97af12ac2a07972e1253f601cc1c39fae
wai-yanphyo <waiyanphyo830@gmail.com>
2025-07-18 09:12:24 +0100

    to change the name

--- app/app.py
+++ app/app.py
@@ -124,7 +124,7 @@ HTML_TEMPLATE = """
         <div class="subtitle">Azure Automated Deployment Complete</div>
         
         <div class="success-badge">
-            Congratulations, Wai Yan!
+            Congratulations, Wai Yan Phyo!
         </div>
         
         <div class="professor-thanks">
